7539c508adf4e027f68cde500d68a4b7b3a4f96b
Axddi <aaditya.saxena.1357@gmail.com>
2025-12-17 18:26:52 +0530

    Add terraform and IAM script for Lambda

---
+++ terraform/iam.tf
@@ -0,0 +1,54 @@
+########################################
+# Trust policy: Allow Lambda to assume role
+########################################
+data "aws_iam_policy_document" "lambda_trust_policy" {
+  statement {
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+
+    actions = ["sts:AssumeRole"]
+  }
+}
+
+########################################
+# IAM Role for Cost Collector Lambda
+########################################
+resource "aws_iam_role" "lambda_cost_role" {
+  name               = "lambda-cost-collector-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust_policy.json
+}
+
+########################################
+# IAM Policy: Cost Explorer read access
+########################################
+data "aws_iam_policy_document" "cost_explorer_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "ce:GetCostAndUsage"
+    ]
+
+    resources = ["*"]
+  }
+}
+
+########################################
+# Create IAM Policy
+########################################
+resource "aws_iam_policy" "cost_policy" {
+  name   = "lambda-cost-explorer-policy"
+  policy = data.aws_iam_policy_document.cost_explorer_policy.json
+}
+
+########################################
+# Attach Policy to Lambda Role
+########################################
+resource "aws_iam_role_policy_attachment" "attach_cost_policy" {
+  role       = aws_iam_role.lambda_cost_role.name
+  policy_arn = aws_iam_policy.cost_policy.arn
+}
