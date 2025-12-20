811e7c282a5f2d5ce9864e3db3d7766baa30c92b
Axddi <aaditya.saxena.1357@gmail.com>
2025-12-21 01:16:24 +0530

    React dashboard for cloud cost visualization

---
+++ dashboard/cloud-cost-dashboard/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Cloud Cost Dashboard</h1>
+      <p>AWS cost data will appear here.</p>
+    </div>
+  );
+}
+
+export default App;
+
